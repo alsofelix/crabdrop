f54df50aafdfbc81733e80e58048ff9bbde365b5
Felix <felixgamblingman@gmail.com>
2026-01-29 19:55:46 +0000

    Fix Linux compatibility by disabling WebKit DMABuf renderer

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,5 +3,11 @@
 #[tokio::main]
 
 async fn main() {
+    #[cfg(target_os = "linux")]
+    {
+        if std::env::var_os("WEBKIT_DISABLE_DMABUF_RENDERER").is_none() {
+            std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+        }
+    }
     crabdrop_lib::run()
 }
