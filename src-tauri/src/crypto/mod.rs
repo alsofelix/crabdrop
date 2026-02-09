fb21f9ad8a08b282b48429d811b9f5423a1bc7de
Felix <felixgamblingman@gmail.com>
2026-02-09 11:27:03 +0000

    decrypt per 1mb + encrypt per 1mb

--- src-tauri/src/crypto/mod.rs
+++ src-tauri/src/crypto/mod.rs
@@ -15,23 +15,42 @@ pub fn derive_key(password: &[u8], salt: &[u8]) -> anyhow::Result<[u8; 32]> {
 pub fn encrypt(data: &mut Vec<u8>, password: &[u8], salt: &[u8]) -> anyhow::Result<String> {
     let key = derive_key(password, salt)?;
     let cipher = XChaCha20Poly1305::new_from_slice(&key).map_err(|e| anyhow!("{e}"))?;
-    let nonce = XChaCha20Poly1305::generate_nonce(&mut OsRng);
-    cipher
-        .encrypt_in_place(&nonce, b"", data)
-        .map_err(|e| anyhow!("{e}"))?;
-    data.splice(0..0, nonce);
+    let mut encrypted_dat: Vec<u8> = Vec::new();
+
+    for (i, chunk) in data.chunks(1024 * 1024).enumerate() {
+        let mut buf = chunk.to_vec();
+
+        let nonce = XChaCha20Poly1305::generate_nonce(&mut OsRng);
+
+        cipher
+            .encrypt_in_place(&nonce, b"", &mut buf)
+            .map_err(|e| anyhow!("{e}"))?;
+
+        buf.splice(0..0, nonce);
+
+        encrypted_dat.extend(buf.clone());
+    }
+
+    *data = encrypted_dat;
     Ok(Uuid::new_v4().to_string())
 }
 
 pub fn decrypt(data: &mut Vec<u8>, password: &[u8], salt: &[u8]) -> anyhow::Result<()> {
     let key = derive_key(password, salt)?;
-    let nonce_bytes: [u8; 24] = data[..24].try_into()?;
-    let nonce = XNonce::from_slice(&nonce_bytes);
-    data.drain(..24);
-    let cipher = XChaCha20Poly1305::new_from_slice(&key)?;
-    cipher.decrypt_in_place(nonce, b"", data).map_err(|e| {
-        println!("{}", e.to_string());
-        anyhow::anyhow!("{e}")
-    })?;
+    let mut encrypted_dat: Vec<u8> = Vec::new();
+
+    for chunk in data.chunks(24 + (1024 * 1024) + 16) {
+        let mut buf = chunk.to_vec();
+        let nonce_bytes: [u8; 24] = buf[..24].try_into()?;
+        buf.drain(..24);
+
+        let nonce = XNonce::from_slice(&nonce_bytes);
+        let cipher = XChaCha20Poly1305::new_from_slice(&key)?;
+        cipher
+            .decrypt_in_place(nonce, b"", &mut buf)
+            .map_err(|e| anyhow::anyhow!("{e}"))?;
+        encrypted_dat.extend(buf);
+    }
+    *data = encrypted_dat;
     Ok(())
 }
