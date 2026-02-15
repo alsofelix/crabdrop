8de91e2b660c4a1581594fc28598e0affa6d1b8c
Felix <felixgamblingman@gmail.com>
2026-02-15 01:10:50 +0000

    chore: bump to v0.2.1

---
+++ bump.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+bun pm version "$1" --no-git-tag-version
+VERSION=$(bun -e "console.log(require('./package.json').version)")
+cd src-tauri
+cargo bump "$1"
+cd ..
+git add -A
+git commit -m "chore: bump to v$VERSION"
+git tag "v$VERSION"
+echo "Run 'git push && git push origin v$VERSION' to release"
