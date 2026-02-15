ac11cb1748a5372150657a8ccbaab8c5b6603396
Felix <felixgamblingman@gmail.com>
2026-02-15 01:49:23 +0000

    chore: update dependencies and bump script for npm lockfile v2 compatibility

--- bump.sh
+++ bump.sh
@@ -2,6 +2,7 @@
 set -e
 bun pm version "$1" --no-git-tag-version
 VERSION=$(bun -e "console.log(require('./package.json').version)")
+npm install --package-lock-only --lockfile-version 2
 cd src-tauri
 cargo bump "$1"
 cargo generate-lockfile
